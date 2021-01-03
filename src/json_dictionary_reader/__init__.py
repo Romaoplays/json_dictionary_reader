addd872b9815d2de539cee4765bac3291affedd8
Gabriel Romao <romaoplays@hotmail.com>
2021-01-03 16:48:32 -0300

    Added get_en_definition

--- src/json_dictionary_reader/__init__.py
+++
@@ -1,6 +0,0 @@
-"""json_dictionary_reader
-By Gabriel Romao romaoplays@hotmail.com
-
-A JSON language dictionaryr eader"""
-
-__version__ = '0.1.0'
--- src/json_dictionary_reader/__main__.py
+++
@@ -1,4 +0,0 @@
-import json_dictionary_reader
-
-if __name__ == '__main__':
-    pass
